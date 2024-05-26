ee4b4c3bd3b642176a3703a6ba5ecdeadba37b6a
yepsamii <abrahmansw@gmail.com>
2024-05-27 01:43:24 +0600

    chore: Add React Router and setup landing page route

--- raihan-folio/src/App.jsx
+++ raihan-folio/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LandingPageSection from "./pages/Landing-Page-Section";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route
+          path="/"
+          element={<LandingPageSection />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ raihan-folio/src/pages/Landing-Page-Section.jsx
@@ -0,0 +1,10 @@
+
+const LandingPageSection = () => {
+    return (
+        <div>
+            Landing Page
+        </div>
+    );
+};
+
+export default LandingPageSection;
