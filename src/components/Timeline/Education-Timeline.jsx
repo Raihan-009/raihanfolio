5977fd7b2a941c3a626bc481214238e374aeef62
Raihan-009 <islamraihan009@gmail.com>
2024-07-07 10:11:30 +0600

    workflow added

--- src/components/Timeline/Education-Timeline.jsx
+++ src/components/Timeline/Education-Timeline.jsx
@@ -7,13 +7,13 @@ const EducationTimeline = ({ edu }) => {
       {edu?.map((education, index) => (
         <Timeline.Item key={index} className="pl-5">
           <Timeline.Point className="w-2.5 h-2.5 -left-[5px] bg-[#3959C1] border-none" />
-          <Timeline.Content className="flex flex-col gap-1">
+          {/* <Timeline.Content className="flex flex-col gap-1">
             <p className="text-sm ">{education?.date}</p>
             <p className="text-sm ">{education?.location}</p>
             <p className="text-lg font-bold ">{education?.role}</p>
             <h6 className="text-2xl font-bold ">{education?.title}</h6>
             <p className=" ">{education?.description}</p>
-          </Timeline.Content>
+          </Timeline.Content> */}
         </Timeline.Item>
       ))}
     </Timeline>
